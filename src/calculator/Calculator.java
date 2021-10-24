93eb4dbd0fbd52b713ac8740149d3e3df7248228
tron200 <geniusgen27300@gmail.com>
2021-10-24 11:46:00 +0100

    first upload

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import javax.swing.JFrame;
+
+public class Calculator extends JFrame{
+    public Calculator(){
+        
+    }
+    
+    public static void main(String[] args) {
+        new Calculator();
+    }
+    
+}
