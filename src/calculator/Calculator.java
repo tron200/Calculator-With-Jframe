26eaa44730fa22f60b0d5faae9ae03c5cea34bbb
tron200 <geniusgen27300@gmail.com>
2021-10-25 21:15:47 +0100

    layout

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,8 +6,16 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.Expression;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,6 +24,7 @@ import javax.swing.WindowConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.AncestorListener;
 
 
 public class Calculator extends JFrame{
@@ -42,7 +51,9 @@ public class Calculator extends JFrame{
         JPanel row2 = new JPanel();
         JPanel row3 = new JPanel();
         JPanel row4 = new JPanel();
-        JTextField screen = new JTextField("hi");
+        JTextField screen = new JTextField("");
+        
+        boolean result = false;
         
         
     public Calculator(){
@@ -97,6 +108,118 @@ public class Calculator extends JFrame{
         BMulti.setFont(new Font("Serif", Font.BOLD, 22));
         BDevide.setPreferredSize(new Dimension(100,75));
         BDevide.setFont(new Font("Serif", Font.BOLD, 22));
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        
+        //Button's Events
+        BZero.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"0");
+            }
+        });
+        BOne.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"1");
+            }
+        });
+        BTwo.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"2");
+            }
+        });
+        BThree.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"3");
+            }
+        });
+        BFour.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"4");
+            }
+        });
+        BFive.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"5");
+            }
+        });
+        BSix.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"6");
+            }
+        });
+        BSeven.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"7");
+            }
+        });
+        BEight.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"8");
+            }
+        });
+        BNine.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"9");
+            }
+        });
+        BPlus.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"+");
+            }
+        });
+        BMines.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"-");
+            }
+        });
+        BMulti.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"*");
+            }
+        });
+        BDevide.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                screen.setText(screen.getText()+"/");
+            }
+        });
+        BDot.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String s = screen.getText();
+                if(!s.contains(".") && !s.isEmpty()){
+                    screen.setText(screen.getText()+".");
+                }
+            }
+        });
+        BEqual.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                try {
+                    String s = screen.getText();
+                    if(!s.isEmpty()){
+                        screen.setText(engine.eval(screen.getText()).toString());
+                        result = true;
+                    }
+                } catch (ScriptException ex) {
+                    Logger.getLogger(Calculator.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        
         
         //setLayout(new GridLayout(0, 1));
         //Calculator Buttons
